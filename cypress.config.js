a12cfc1e865006920a254594f4f591895fd5c248
wani5272 <205021230+wani5272@users.noreply.github.com>
2025-04-28 11:29:20 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   cypress.config.js
    	modified:   cypress/e2e/spec.cy.js
    	new file:   cypress/screenshots/spec.cy.js/template spec -- bfo-website (failed).png

--- cypress.config.js
+++ cypress.config.js
@@ -8,3 +8,19 @@ export default defineConfig({
     video: true,
   },
 });
+// Throttle API responses to simulate real-world conditions
+beforeEach(() => {
+  cy.intercept(
+    {
+      url: 'http://localhost:3001/**',
+      middleware: true,
+    },
+    (req) => {
+      req.on('response', (res) => {
+        // Throttle the response to 1 Mbps to simulate a
+        // mobile 3G connection
+        res.setThrottle(1000)
+      })
+    }
+  )
+})
