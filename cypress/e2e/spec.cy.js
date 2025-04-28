2a6ba2327b1e2304879c81a3b173d0e178ac9328
wani5272 <205021230+wani5272@users.noreply.github.com>
2025-04-28 10:29:04 +0200

    finish test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,18 @@
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://berufsbildung-vs.ch");
-    cy.viewport(1920, 2080);
+    cy.viewport(1920, 1080);
     cy.get(
       '#nav-21 > [href="https://www.berufsbildung-vs.ch/berufslehre"] > span'
     ).click();
     cy.get("#listing_search_input_49").type("Informatik");
     cy.get(".button").click();
-    cy.contains("Informatiker/in EFZ (EMVs)").scrollIntoView();
-    cy.contains(".listing-entry-inline", "EMVs")
+    cy.contains(".listing-entry-inline", "Informatiker/in EFZ (EMVs)")
       .find('[class="listing_entry_link"]')
       .click();
+    cy.contains("Voraussetzungen");
+    cy.contains("Schnuppertage");
+    cy.contains("Anmeldung");
+    cy.get(".resource_background > img").should("be.visible");
   });
 });
